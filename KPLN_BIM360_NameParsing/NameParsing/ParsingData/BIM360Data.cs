2b05f247356f902ffb93beaea672d3d47c51296c
Tsimafei Kutsko <tima.kutsko@gmail.com>
2021-12-20 13:55:03 +0300

    Дебагинг

--- KPLN_BIM360_NameParsing/NameParsing/ParsingData/BIM360Data.cs
+++ KPLN_BIM360_NameParsing/NameParsing/ParsingData/BIM360Data.cs
@@ -1,7 +1,5 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
